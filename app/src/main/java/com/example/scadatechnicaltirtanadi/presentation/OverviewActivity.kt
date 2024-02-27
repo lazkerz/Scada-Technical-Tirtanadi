050c42e4e3d59dbcf012debdf5bfe3863d5515ea
Lazkerz <Laziranurul@gmail.com>
2024-02-27 08:40:15 +0700

    bug: fix overview with token header

--- app/src/main/java/com/example/scadatechnicaltirtanadi/presentation/OverviewActivity.kt
+++ app/src/main/java/com/example/scadatechnicaltirtanadi/presentation/OverviewActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.webkit.WebResourceRequest
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
@@ -23,6 +24,7 @@ import java.net.URL
 class OverviewActivity : AppCompatActivity(), user_view {
 
     private lateinit var presenter: UserData
+    private lateinit var webView: WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,17 +32,13 @@ class OverviewActivity : AppCompatActivity(), user_view {
 
         val apiService = ApiConfig.getToken(this)
         if (apiService != null) {
-            presenter = UserData(
-                apiService,
-                this,
-            )
+            presenter = UserData(apiService, this)
         } else {
             Log.e("SignIn", "Failed to initialize ApiService")
-            Toast.makeText(this, "Failed to initialize ApiService", Toast.LENGTH_SHORT)
-                .show()
+            Toast.makeText(this, "Failed to initialize ApiService", Toast.LENGTH_SHORT).show()
         }
 
-        val webView: WebView = findViewById(R.id.overview)
+        webView = findViewById(R.id.overview)
         val back = findViewById<ImageView>(R.id.back)
 
         back.setOnClickListener {
@@ -50,54 +48,16 @@ class OverviewActivity : AppCompatActivity(), user_view {
             finish()
         }
 
-
-        val accessToken = presenter.getAccessToken()
+        val accessToken = presenter.getRefreshToken()
+        Log.d("Token:", "${accessToken}")
         val branchSlug = intent.getStringExtra("branch_slug") ?: ""
 
-        webView.webViewClient = WebViewClient()
-        webView.loadUrl("https://scada-technical.tirtanadi.ibmsindo.net/${branchSlug}/overview")
+        webView.webViewClient = CustomWebViewClient()
 
-        val webSettings = webView.settings
-        webSettings.javaScriptEnabled = true
-        webSettings.domStorageEnabled = true
-        webSettings.setSupportZoom(true) // Aktifkan kontrol zoom
-        webSettings.builtInZoomControls = true // Aktifkan kontrol zoom bawaan WebView
-        webSettings.displayZoomControls = false
-   
-
-
-
-//        val url = "https://scada-technical.tirtanadi.ibmsindo.net/${branchSlug}/overview"
-//
-//        val webViewClient = object : WebViewClient() {
-//            override fun shouldOverrideUrlLoading(view: WebView?, url: String): Boolean {
-//                // Tambahkan header permintaan HTTP
-//                val connection = URL(url).openConnection() as HttpURLConnection
-////                connection.setRequestProperty("Authorization: ", "Bearer $accessToken")
-//                try {
-//                    connection.connect()
-//                    view?.loadUrl(url)
-//                } catch (e: IOException) {
-//                    // Tangani kesalahan
-//                    e.printStackTrace()
-//                } finally {
-//                    connection.disconnect()
-//                }
-//                return true
-//            }
-//        }
-//
-//        webView.settings.apply {
-//            javaScriptEnabled = true
-//            setSupportZoom(false)
-//            builtInZoomControls = false
-//            displayZoomControls = false
-//            loadWithOverviewMode = true
-//            cacheMode = WebSettings.LOAD_DEFAULT
-//        }
-//
-//        webView.webViewClient = webViewClient
-//        webView.loadUrl(url)
+        // Tambahkan AccessToken ke header WebView
+        val headers = HashMap<String, String>()
+        headers["Authorization"] = "Bearer $accessToken"
+        webView.loadUrl("https://scada-technical.tirtanadi.ibmsindo.net/${branchSlug}/overview", headers)
     }
 
     override fun onLogin(result: ResultState<AccessToken>) {
@@ -114,6 +74,13 @@ class OverviewActivity : AppCompatActivity(), user_view {
                 // Handle loading state
                 Toast.makeText(this, "Loading..", Toast.LENGTH_SHORT).show()
             }
+        }
+    }
+
+    private inner class CustomWebViewClient : WebViewClient() {
+        override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+            // Handle URL loading here if needed
+            return super.shouldOverrideUrlLoading(view, request)
         }
     }
 }
